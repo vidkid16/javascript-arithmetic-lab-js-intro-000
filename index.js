0d106001b93261a4fc44bc8a0891fcf996f86088
Eliot Garcia <eag0828@gmail.com>
2018-02-26 22:58:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function add(a, b) {
+  return a += b
+}
+add()
+
+function subtract(a, b) {
+  return a -= b
+}
+subtract()
+
+function multiply(a, b) {
+  return a *= b
+}
+multiply()
+
+function divide(a, b) {
+  return a /= b
+}
+divide()
+
+function inc(n) {
+  return ++n
+}
+inc()
+
+function dec(n) {
+  return --n
+}
+dec()
+
+function makeInt(n) {
+  parseInt('n', )
+  return n
+}
